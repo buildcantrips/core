0426bd6a84079948dd99168df1461cc63291cb48
Csaba Makara <csaba.makara@emarsys.com>
2018-11-14 21:14:00 +0100

    feat(teamcity): added basic TeamCity support

--- src/ParameterProvider.js
+++ src/ParameterProvider.js
@@ -1,12 +1,14 @@
-"use strict";
-
 function determineCiServer() {
   if (process.env.CIRCLECI) {
     return "CircleCi";
   }
+  if (process.env.TEAMCITY_VERSION) {
+    return "TeamCity";
+  }
   if (process.env.BUILD_NUMBER) {
     return "Jenkins";
   }
+
   throw Error("Unknown CI server environment");
 }
 
@@ -14,51 +16,54 @@ export class ParameterProvider {
   constructor() {
     this.ciServer = determineCiServer();
     this.parameterMap = {
-      DockerTarget: {
-        CircleCi: process.env.DOCKER_TARGET || "",
-        Jenkins: process.env.DOCKER_TARGET || ""
+      DockerRegistry: {
+        CircleCi: process.env.DOCKER_REGISTRY || "",
+        Jenkins: process.env.DOCKER_REGISTRY || "",
+        TeamCity: process.env.DOCKER_REGISTRY || ""
       },
       Tag: {
         CircleCi: process.env.CIRCLE_TAG || "",
-        Jenkins: process.env.BRANCH_NAME || ""
+        Jenkins: process.env.BRANCH_NAME || "",
+        TeamCity: process.env.BRANCH_NAME || ""
       },
       ReleaseTagFormat: {
         CircleCi: process.env.RELEASE_TAG_FORMAT || "release-",
-        Jenkins: process.env.RELEASE_TAG_FORMAT || "release-"
+        Jenkins: process.env.RELEASE_TAG_FORMAT || "release-",
+        TeamCity: process.env.RELEASE_TAG_FORMAT || "release-"
       },
       BranchName: {
         CircleCi: process.env.CIRCLE_BRANCH || "",
-        Jenkins: process.env.BRANCH_NAME || ""
+        Jenkins: process.env.BRANCH_NAME || "",
+        TeamCity: process.env.BRANCH_NAME || ""
       },
       BuildNumber: {
         CircleCi: "",
-        Jenkins: process.env.BUILD_NUMBER || ""
+        Jenkins: process.env.BUILD_NUMBER || "",
+        TeamCity: process.env.BUILD_NUMBER || ""
       }
     };
-    this.computeDerivedParamters();
+    this.computeDerivedParameters();
   }
 
-  computeDerivedParamters() {
+  computeDerivedParameters() {
+    let isRelease =
+      this.getParameter("Tag") &&
+      this.getParameter("Tag").startsWith(this.getParameter("Tag"));
+
+    let releaseVersion = this.getParameter("Tag").replace(
+      this.getParameter("ReleaseTagFormat"),
+      ""
+    );
     this.parameterMap = Object.assign({}, this.parameterMap, {
       IsRelease: {
-        CircleCi:
-          this.getParameter("Tag") &&
-          this.getParameter("Tag").startsWith(
-            this.getParameter("ReleaseTagFormat")
-          ),
-        Jenkins:
-          this.getParameter("Tag") &&
-          this.getParameter("Tag").startsWith(
-            this.getParameter("ReleaseTagFormat")
-          )
+        CircleCi: isRelease,
+        Jenkins: isRelease,
+        TeamCity: isRelease
       },
       ReleaseVersion: {
-        CircleCi:
-          this.getParameter("Tag").replace(
-            this.getParameter("ReleaseTagFormat"),
-            ""
-          ) || "",
-        Jenkins: process.env.BRANCH_NAME
+        CircleCi: releaseVersion,
+        Jenkins: process.env.BRANCH_NAME || "",
+        TeamCity: releaseVersion
       },
       ProjectName: {
         CircleCi: () => {
@@ -66,7 +71,12 @@ export class ParameterProvider {
             .env.CIRCLE_PROJECT_REPONAME || ""}`;
           return result !== "/" ? result : "unknown";
         },
-        Jenkins: process.env.JOB_NAME || ""
+        Jenkins: process.env.JOB_NAME || "",
+        TeamCity: () => {
+          const result = `${process.env.PROJECT_GROUP || ""}/${process.env
+            .PROJECT_NAME || ""}`;
+          return result !== "/" ? result : "unknown";
+        }
       }
     });
   }
--- src/ParameterProvider.test.js
+++ src/ParameterProvider.test.js
@@ -5,15 +5,37 @@ import { expect } from "chai";
 
 describe("parameterProvider", () => {
   var provider;
-  beforeAll(() => {
-    process.env.CIRCLECI = "CIRCLECI";
-    process.env.CIRCLE_BRANCH = "validBranchName";
-    process.env.CIRCLE_PROJECT_USERNAME = "validUser";
-    process.env.CIRCLE_PROJECT_REPONAME = "validRepoName";
-    provider = new ParameterProvider();
+
+  it("throws error using in unknown environment", async () => {
+    expect(() => new ParameterProvider())
+      .to.throw(Error)
+      .that.ownProperty("message")
+      .which.includes("Unknown CI");
+  });
+
+  describe("core", () => {
+    beforeAll(() => {
+      process.env.CIRCLECI = "CIRCLECI";
+      provider = new ParameterProvider();
+    });
+    describe("getParameter", () => {
+      it("throws error on asking for not existing parameter", async () => {
+        expect(() => provider.getParameter("notExistingParameter"))
+          .to.throw(Error)
+          .that.ownProperty("message")
+          .which.includes("Unknown parameter");
+      });
+    });
   });
   describe("environments", () => {
     describe("CircleCI", () => {
+      beforeAll(() => {
+        process.env.CIRCLECI = "CIRCLECI";
+        process.env.CIRCLE_BRANCH = "validBranchName";
+        process.env.CIRCLE_PROJECT_USERNAME = "validUser";
+        process.env.CIRCLE_PROJECT_REPONAME = "validRepoName";
+        provider = new ParameterProvider();
+      });
       it("resolves variables", async () => {
         expect(await provider.getParameter("BranchName")).to.be.eql(
           "validBranchName"
@@ -25,13 +47,44 @@ describe("parameterProvider", () => {
         );
       });
     });
-  });
-  describe("getParameter", () => {
-    it("throws error on asking for not existing parameter", async () => {
-      expect(() => provider.getParameter("notExistingParameter"))
-        .to.throw(Error)
-        .that.ownProperty("message")
-        .which.includes("Unknown parameter");
+    describe("TeamCity", () => {
+      beforeAll(() => {
+        process.env.CIRCLECI = "";
+        process.env.TEAMCITY_VERSION = "2018.1";
+        process.env.BUILD_NUMBER = "1234";
+        process.env.BRANCH_NAME = "validBranchName";
+        process.env.PROJECT_GROUP = "validGroup";
+        process.env.PROJECT_NAME = "validRepoName";
+        provider = new ParameterProvider();
+      });
+      it("resolves branch name", async () => {
+        expect(await provider.getParameter("BranchName")).to.be.eql(
+          "validBranchName"
+        );
+      });
+      it("resolves tag", async () => {
+        expect(await provider.getParameter("Tag")).to.be.eql("validBranchName");
+      });
+      it("resolves build number", async () => {
+        expect(await provider.getParameter("BuildNumber")).to.be.eql("1234");
+      });
+      it("determines release mode correctly", async () => {
+        process.env.BRANCH_NAME = "release-1.2.3";
+        provider = new ParameterProvider();
+        expect(await provider.getParameter("IsRelease")).to.be.eql(true);
+      });
+      it("computes release version correctly", async () => {
+        process.env.BRANCH_NAME = "release-1.2.3";
+        provider = new ParameterProvider();
+        expect(await provider.getParameter("ReleaseVersion")).to.be.eql(
+          "1.2.3"
+        );
+      });
+      it("constructs project name correctly", async () => {
+        expect(await provider.getParameter("ProjectName")).to.be.eql(
+          "validGroup/validRepoName"
+        );
+      });
     });
   });
 });
