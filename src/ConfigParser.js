0a42dcea361b7595a9d1d4e798952d0b1c91b711
Csaba Makara <csaba.makara@emarsys.com>
2018-11-29 17:08:04 +0100

    debug(logger): improve config loader logging

--- src/ConfigParser.js
+++ src/ConfigParser.js
@@ -1,11 +1,19 @@
 import fs from "fs"
 import path from "path"
+import { Logger } from "./Logger"
 
 const CONFIG_PATH = path.join(process.cwd(), ".cantrips")
 
 async function parseConfig() {
   if (fs.existsSync(CONFIG_PATH)) {
-    return JSON.parse(fs.readFileSync(CONFIG_PATH))
+    try {
+      return JSON.parse(fs.readFileSync(CONFIG_PATH))
+    } catch (error) {
+      Logger.debug(error)
+      throw Error(
+        `Parsing configuration file ${CONFIG_PATH} failed.\n${error.message}`
+      )
+    }
   }
 }
 
